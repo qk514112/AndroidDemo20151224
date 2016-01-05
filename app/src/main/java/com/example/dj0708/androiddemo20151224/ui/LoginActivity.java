871bd86a3e37073e7c50696261b2b2a3e67abc0a
dj0708 <qike@pj.com>
2016-01-05 16:06:56 +0800

    1, login layout;

---
+++ app/src/main/java/com/example/dj0708/androiddemo20151224/ui/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.dj0708.androiddemo20151224.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.dj0708.androiddemo20151224.R;
+
+/**
+ * Created by dj0708 on 16/1/4.
+ */
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
