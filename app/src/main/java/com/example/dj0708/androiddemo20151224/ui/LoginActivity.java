dd3388694808ba5c5f79e0c6d125181b7e9e8a8c
dj0708 <qike@pj.com>
2016-01-12 09:57:34 +0800

    1, run request in <runable> get test result message in handler; 2, show test result or exception with <EditText>;

--- app/src/main/java/com/example/dj0708/androiddemo20151224/ui/LoginActivity.java
+++ app/src/main/java/com/example/dj0708/androiddemo20151224/ui/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.dj0708.androiddemo20151224.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 import android.widget.Button;
@@ -15,6 +16,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import android.os.Handler;
+import android.widget.Toast;
 
 /**
  * Created by dj0708 on 16/1/4.
@@ -22,21 +24,34 @@ import android.os.Handler;
 public class LoginActivity extends Activity {
     private EditText       userNameEditText;
     private EditText       passwordEditText;
+    private EditText       responseEditText;
     private Button         loginBtn;
 
-    private final String         KEY_LOGIN_RESPONSE = "loginResponse";
+    private final static String          KEY_LOGIN_RESPONSE = "loginResponse";
 
-    public final static String          LOGIN_URL_STR = "https://www.pj.com/oauth/token";
-    public final static String          REQUEST_CHARSET_TYPE = "UTF-8";
-    public final static int             REQUEST_TIME_OUT = 10000;
+    private final static String          LOGIN_URL_STR = "https://www.pj.com/oauth/token";
+//private final static String          LOGIN_URL_STR = "http://www.baidu.com";
+
+    private final static String          REQUEST_CHARSET_TYPE = "UTF-8";
+    private final static String          BASIC_TOKEN_STR = "Basic MzUzYjMwMmM0NDU3NGY1NjUwNDU2ODdlNTM0ZTdkNmE6Mjg2OTI0Njk3ZTYxNWE2NzJhNjQ2YTQ5MzU0NTY0NmM=";
+    private final static String          JSON_REQUEST_PROPERTY = "application/json";
+    private final static String          WWW_FORM_REQUEST_PROPERTY = "application/x-www-form-urlencoded;charset=UTF-8";
+    private final static String          ACCEPT_CHARSET = "Accept-Charset";
+    private final static String          AUTHORIZATION = "Authorization";
+    private final static String          CONTENT_TYPE = "Content-Type";
+    private final static int             REQUEST_TIME_OUT = 10000;
+
+    private final static String          TEST_USER_NAME = "test";
+    private final static String          TEST_PASSWORD = "123456";
+    private final static String          TEST_OUT_MESSAGE = "test test test";
 
     Handler handler = new Handler() {
         @Override
         public void handleMessage(Message message) {
             super.handleMessage(message);
             Bundle dataBundle = message.getData();
-            String responseStr = dataBundle.getString("loginResponse");
-            System.out.print(responseStr);
+            String responseStr = dataBundle.getString(KEY_LOGIN_RESPONSE);
+            responseEditText.setText(responseStr);
         }
     };
 
@@ -44,19 +59,19 @@ public class LoginActivity extends Activity {
         @Override
         public void run() {
             try {
-                String userNameParameter = URLEncoder.encode("test", "UTF-8");
-                String passwordParameter = URLEncoder.encode("123456", "UTF-8");
+                String userNameParameter = URLEncoder.encode(TEST_USER_NAME, REQUEST_CHARSET_TYPE);
+                String passwordParameter = URLEncoder.encode(TEST_PASSWORD, REQUEST_CHARSET_TYPE);
                 String bodyStr = String.format("username=%s&password=%s&grant_type=password", userNameParameter, passwordParameter);
-                URL url = new URL("http://192.168.1.169:18080/oauth/token");
+                URL url = new URL(LOGIN_URL_STR);
                 HttpURLConnection connection = (HttpURLConnection)url.openConnection();
                 connection.setDoOutput(true);
-                connection.setRequestProperty("Accept-Charset", "application/json");
-                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-                connection.setRequestProperty("Authorization", "Basic MzUzYjMwMmM0NDU3NGY1NjUwNDU2ODdlNTM0ZTdkNmE6Mjg2OTI0Njk3ZTYxNWE2NzJhNjQ2YTQ5MzU0NTY0NmM=");
-                connection.setConnectTimeout(10000);
+                connection.setRequestProperty(ACCEPT_CHARSET, JSON_REQUEST_PROPERTY);
+                connection.setRequestProperty(CONTENT_TYPE, WWW_FORM_REQUEST_PROPERTY);
+                connection.setRequestProperty(AUTHORIZATION, BASIC_TOKEN_STR);
+                connection.setConnectTimeout(REQUEST_TIME_OUT);
 
                 OutputStream outputStream = connection.getOutputStream();
-                outputStream.write(bodyStr.getBytes("UTF-8"));
+                outputStream.write(bodyStr.getBytes(REQUEST_CHARSET_TYPE));
                 InputStream inputStream = connection.getInputStream();
 
                 int status = connection.getResponseCode();
@@ -64,11 +79,15 @@ public class LoginActivity extends Activity {
 
                 Message message = new Message();
                 Bundle dataBundle = new Bundle();
-                dataBundle.putString("loginResponse", "testtest message");
+                dataBundle.putString(KEY_LOGIN_RESPONSE, TEST_OUT_MESSAGE);
                 message.setData(dataBundle);
                 handler.sendMessage(message);
             } catch (Exception exception) {
-                System.out.println("testtest out print");
+                Message message = new Message();
+                Bundle dataBundle = new Bundle();
+                dataBundle.putString(KEY_LOGIN_RESPONSE, exception.toString());
+                message.setData(dataBundle);
+                handler.sendMessage(message);
             }
         }
     };
@@ -79,6 +98,7 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
         userNameEditText = (EditText)findViewById(R.id.userNameEditText);
         passwordEditText = (EditText)findViewById(R.id.passwordEditText);
+        responseEditText = (EditText)findViewById(R.id.responseEditText);
         loginBtn = (Button)findViewById(R.id.loginBtn);
         new Thread(requestLoginRunable).start();
     }
